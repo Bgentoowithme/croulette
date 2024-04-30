d2db9026d26815670fb6a7f3a5161bfd43050708
Stan <167658232+Bgentoowithme@users.noreply.github.com>
2024-04-30 21:16:45 +0100

    Add files via upload
    1st 'stable' build - might add more; might not ¯\_(ツ)_/¯

---
+++ src2.c
@@ -0,0 +1,282 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h> // for sleep function
+#include <time.h> // to use time as seed
+
+#define straightup 35
+#define twotoone 2
+#define onetoone 1
+#define HISTORY_SIZE 10
+
+
+int roulette[] = {0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13,
+                  36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14,
+                  31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26, 0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13,
+                  36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14,
+                  31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26};
+int evens[] = {0, 32, 4, 2, 34, 6, 36, 30, 10, 24, 16, 20};
+int odds[] = {15, 19, 21, 25, 17, 27, 13, 11, 23, 5, 33, 1, 31, 9, 29, 7, 35, 3, 26};
+int reds[] = {32, 15, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3, 26};
+int blacks[] = {0, 4, 2, 17, 6, 11, 8, 10, 24, 20, 33, 31, 29, 35};
+int to18[] = {15, 4, 2, 17, 6, 13, 11, 10, 5, 16, 1, 14, 9, 7, 12, 3};
+int to36[] = {32, 19, 21, 25, 34, 27, 36, 30, 23, 24, 20, 33, 31, 29, 35, 26};
+int col1[] = {32, 4, 2, 17, 6, 13, 11, 10, 5, 16, 1, 14};
+int col2[] = {15, 19, 21, 25, 27, 23, 20, 31, 29, 7, 12, 3};
+int col3[] = {36, 30, 34, 24, 33, 9, 18, 35, 26};
+int dozen1[] = {0, 32, 15, 19, 4, 2, 17, 6, 13, 11, 10, 5};
+int dozen2[] = {16, 36, 30, 8, 23, 24, 20, 14, 31, 9, 22, 18};
+int dozen3[] = {21, 25, 34, 27, 36, 13, 11, 30, 5, 16, 33, 1};
+// could have used enums but Ive only just found out about them
+void graphic(int lnum); // emulates the roulette wheel spining
+int win(char num[], int lnum, int bet, int *money, int *twin, int *tloss); //calculates the win/loss
+void ansicolour(int colour); // to change terminal colour
+void ansicolourbold(int colour); // to change terminal colour but in bold
+int main() {
+    int twin = 0;
+    int tloss = 0;
+    int money = 10;
+    char playagain;
+    int lnum_history[HISTORY_SIZE] = {}; // Array to store the last 10 lnums
+    int history_index = 0;
+    do {
+        int seed, bet;
+        char TOC;
+        char num[5] = "    ";
+        printf("Use time as seed? (y/n): ");
+        scanf(" %c", &TOC);
+
+        if (TOC == 'y' || TOC == 'Y') {
+            srand(time(NULL));
+        } else {
+            printf("Please enter a valid seed value to be used: \n");
+            scanf("%d", &seed);
+            srand(seed);
+        }
+
+        int lnum = rand() % 36 + 0;
+        lnum_history[history_index] = lnum;
+        history_index = (history_index + 1) % HISTORY_SIZE;
+
+        printf("bet?");
+        scanf(" %d", &bet);
+        if(money < bet){
+        printf("You don't have enough");
+        //sets console text colour to red and bold
+        ansicolourbold(31);
+        printf("MONEY");
+        //sets console text back to default
+        ansicolour(0);
+        printf("!");
+        printf(" Please come back once you've made some more :)\n");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("num? (e=evens,)");
+        scanf(" %s", &num);
+
+        printf("-----\n");
+        graphic(lnum);
+        printf("| %d |", lnum);
+        printf("\n-----\n");
+        win(num, lnum, bet, &money, &twin, &tloss);
+
+        printf("\n Your money: %d\n", money);
+// Print out lnum_history with colors (using ANSI escape codes)
+printf("Last 10 lnums: ");
+for (int i = 0; i < HISTORY_SIZE; i++) {
+    if (lnum_history[i] == 0) {
+        ansicolour(32);
+        printf("\033[0;32m%d \033[0m", lnum_history[i]);
+    } else {
+        // Check if the number is red
+        int isRed = 0;
+        for (int j = 0; j < sizeof(reds) / sizeof(reds[0]); j++) {
+            if (lnum_history[i] == reds[j]) {
+                isRed = 1;
+                break;
+            }
+        }
+        if (isRed) {
+            //print red numbers in red
+            ansicolour(31);
+            printf("\033[0;31m%d \033[0m", lnum_history[i]);
+        } else {
+            //print other numbers in default colour
+            ansicolour(0);
+            printf("%d ", lnum_history[i]);
+        }
+    }
+}
+printf("\n");
+printf("\n total win %d\n total loss %d",twin , tloss);
+
+        printf("\nDo you want to play again? (y/n): ");
+        scanf(" %c", &playagain);
+    } while (playagain == 'y' || playagain == 'Y');
+
+    printf("Thanks for playing! Come again :)\n");
+    return 0;
+
+}
+
+void graphic(int lnum) {
+    int n = rand() % 36 + 0;
+    do {
+        printf("| %d |\r", roulette[n]);
+        fflush(stdout);
+        usleep(500000);
+        n++;
+    } while (lnum != roulette[n]);
+}
+
+
+int win(char num[5], int lnum, int bet, int *money, int *twin, int *tloss) {
+    printf("num is %s", num); // debug print
+    if ((strncmp(num, "e", 1) != 0 || strncmp(num, "o", 1) != 0 || strncmp(num, "r", 1) != 0 || strncmp(num, "b", 1) != 0 || strncmp(num, "to18", 4) != 0 || strncmp(num, "to36", 4) != 0 || strncmp(num, "col1", 4) != 0 || strncmp(num, "col2", 4) != 0 || strncmp(num, "col3", 4) != 0 || strncmp(num, "doz1", 4) != 0 || strncmp(num, "doz2", 4) != 0 || strncmp(num, "doz3", 4) != 0) && atoi(num) == lnum) {
+        printf("\ncongrats");
+        *money = *money + straightup * bet;
+        *twin = *twin + bet * straightup;
+        return *twin;
+        return *money;
+    } else if (strncmp(num, "e", 1) == 0) {
+        for (int i = 0; i < sizeof(evens) / sizeof(evens[0]); i++) {
+            if (evens[i] == lnum) {
+                printf("\nCurrent money: %d, Bet: %d", *money, bet); // Debug print
+                printf("\ncongrats");
+                *money = *money + bet * onetoone;
+                *twin = *twin + bet * onetoone;
+                printf("\nUpdated money: %d", *money); // Debug print
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "o", 1) == 0) {
+        for (int i = 0; i < sizeof(odds) / sizeof(odds[0]); i++) {
+            if (odds[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * onetoone;
+                *twin = *twin + bet * onetoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "r", 1) == 0) {
+        for (int i = 0; i < sizeof(reds) / sizeof(reds[0]); i++) {
+            if (reds[i] == lnum) {
+                printf("\nCurrent money: %d, Bet: %d", *money, bet); // Debug print
+                printf("\ncongrats");
+                *money = *money + bet * onetoone;
+                *twin = *twin + bet * onetoone;
+                printf("\nUpdated money: %d", *money); // Debug print
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "b", 1) == 0) {
+        for (int i = 0; i < sizeof(blacks) / sizeof(blacks[0]); i++) {
+            if (blacks[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * onetoone;
+                *twin = *twin + bet * onetoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "to18", 4) == 0) {
+        for (int i = 0; i < sizeof(to18) / sizeof(to18[0]); i++) {
+            if (to18[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * onetoone;
+                *twin = *twin + bet * onetoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "to36", 4) == 0) {
+        for (int i = 0; i < sizeof(to36) / sizeof(to36[0]); i++) {
+            if (to36[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * onetoone;
+                *twin = *twin + bet * onetoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "col1", 4) == 0) {
+        for (int i = 0; i < sizeof(col1) / sizeof(col1[0]); i++) {
+            if (col1[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * twotoone;
+                *twin = *twin + bet * twotoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "col2", 4) == 0) {
+        for (int i = 0; i < sizeof(col2) / sizeof(col2[0]); i++) {
+            if (col2[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * twotoone;
+                *twin = *twin + bet * twotoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "col3", 4) == 0) {
+        for (int i = 0; i < sizeof(col3) / sizeof(col3[0]); i++) {
+            if (col3[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * twotoone;
+                *twin = *twin + bet * twotoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "doz1", 4) == 0) {
+        for (int i = 0; i < sizeof(dozen1) / sizeof(dozen1[0]); i++) {
+            if (dozen1[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * twotoone;
+                *twin = *twin + bet * twotoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "doz2", 4) == 0) {
+        for (int i = 0; i < sizeof(dozen2) / sizeof(dozen2[0]); i++) {
+            if (dozen2[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * twotoone;
+                *twin = *twin + bet * twotoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } else if (strncmp(num, "doz3", 4) == 0) {
+        for (int i = 0; i < sizeof(dozen3) / sizeof(dozen3[0]); i++) {
+            if (dozen3[i] == lnum) {
+                printf("\ncongrats");
+                *money = *money + bet * twotoone;
+                *twin = *twin + bet * twotoone;
+                return *money;
+                return *twin;
+            }
+        }
+    } 
+    printf("\nbetter luck next time");
+        printf("\nCurrent money: %d, Bet: %d", *money, bet); // Debug print
+        *money = *money - bet;
+        *tloss = *tloss + bet;
+        printf("\nUpdated money: %d", *money); // Debug print
+    return *money;
+    return *tloss;
+}
+
+void ansicolour(int colour) {
+    printf("\033[0;%dm", colour);
+}
+
+void ansicolourbold(int colour) {
+    printf("\033[1;%dm", colour);
+}
+
